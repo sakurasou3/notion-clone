ef00b481adaae643c9ed1abf880fa05e4c9adc6d
sakurasou3 <ko.ki.0216@gmail.com>
2024-02-29 09:06:24 +0900

    fix header

---
+++ client/src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthLayout } from "./components/layout/AuthLayout";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { blue } from "@mui/material/colors";
+
+function App() {
+  const theme = createTheme({
+    palette: { primary: blue },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<AuthLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ client/src/components/layout/AuthLayout.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Container } from "@mui/system";
+import Logo from "../../assets/images/notion-logo.png";
+
+export const AuthLayout = () => {
+  return (
+    <div>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 6,
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <img
+            src={Logo}
+            alt="logo"
+            style={{ width: 100, height: 100, marginBottom: 3 }}
+          />
+          Notionクローン開発
+        </Box>
+      </Container>
+      <Outlet />
+    </div>
+  );
+};
