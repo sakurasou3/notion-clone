0653b60398a87ea96d7e32b487da500cdf9bd527
sakurasou3 <ko.ki.0216@gmail.com>
2024-03-08 09:25:32 +0900

    fix

--- client/src/components/layout/AuthLayout.tsx
+++ client/src/components/layout/AuthLayout.tsx
@@ -1,11 +1,8 @@
 import React, { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
-import React, { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
 import { Box } from "@mui/material";
 import { Container } from "@mui/system";
 import Logo from "../../assets/images/notion-logo.png";
-import { authUtils } from "../../utils/authUtils";
 import { authUtils } from "../../utils/authUtils";
 
 export const AuthLayout = () => {
